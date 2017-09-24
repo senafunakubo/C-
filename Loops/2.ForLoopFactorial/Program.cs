6944fcf225f3c94da19e610bb17ca40f48d8d02d
Sena <inabacker24@gmail.com>
2017-09-23 23:52:43 -0700

    Add loops

---
+++ Loops/2.ForLoopFactorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ForLoopFactorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Factrial 2! = 2*1 = 2;
+            //Factrial 3! = 3*2*1 = 6;
+            //Factrial 4! = 4*3*2*1 = 24;
+            //Factrial 5! = 5*4*3*2*1 = 120;
+
+            //intialization; condition; increment/decrement
+            int n = 5;
+            int factrial = 1;
+            for (int i = n; i >= 1; i--){
+                factrial = factrial * i;
+            }
+            Console.WriteLine("{0}! = {1}", n, factrial);
+
+			int m = 20;
+            decimal fac = 1;
+			for (int i = m; i >= 1; i--)
+			{
+				fac = fac * i;
+			}
+			Console.WriteLine("{0}! = {1}", m, fac);
+
+            //Variation of for loop
+            //For-each loop
+            string[] dayName = new string[]{
+                "MONDAY","TUESDAY","WEDNESDAY",
+                "THURSDAY","FRIDAY","SATURDAY","SUNDAY"
+            };
+            //datatype variable keyword(in) collection_name
+            //for-each is used over collection i.g. list, arrays
+            //maps,sets
+            //foreach(string day)
+
+            Console.Read();
+		}
+    }
+}
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1.WhileLoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Entry controlled loop
+            //1. Intialization
+            //2. Condition
+            //3. Increment/ Decrement
+            int i = 1;
+            Console.WriteLine("series from 1 to 50");
+
+            while(i<=50){
+                Console.Write("\t{0}",i);
+                i++;
+            }
+            //?how many time??? 50 times
+            //exit Controlled loop Do...While();
+            int j = 1;
+            Console.WriteLine("Using Do while loop series from 1 to 50");
+            do
+            {
+                Console.Write("\t{0}", j);
+                j++;
+            } while (j <= 50);
+            Console.Read();
+        }
+    }
+}
