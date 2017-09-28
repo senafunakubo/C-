b6dd76148d00c3746db21322b13c42751ccb7a26
Sena <inabacker24@gmail.com>
2017-09-27 23:23:52 -0700

    Add stackLayout ex

---
+++ StackLayoutEx/StackLayoutEx/StackLayoutEx2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StackLayoutEx
+{
+    public partial class StackLayoutEx2 : ContentPage
+    {
+        public StackLayoutEx2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StackLayoutEx/StackLayoutEx/StackLayoutExPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StackLayoutEx
+{
+    public partial class StackLayoutExPage : ContentPage
+    {
+        public StackLayoutExPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
