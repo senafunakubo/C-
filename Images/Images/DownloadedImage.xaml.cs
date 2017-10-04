5b62e35c2e3cf3d9dedd804f96372953011111c9
Sena <inabacker24@gmail.com>
2017-10-04 12:43:41 -0700

    add files
    deleted Droid and iOS

---
+++ Images/Images/DownloadedImage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Images
+{
+    public partial class DownloadedImage : ContentPage
+    {
+        public DownloadedImage()
+        {
+            InitializeComponent();
+            embeddedImage.Source = ImageSource.FromResource("Images.beach.jpg");
+        }
+    }
+}
---
+++ Images/Images/ImagesPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Images
+{
+    public partial class ImagesPage : ContentPage
+    {
+        public ImagesPage()
+        {
+            InitializeComponent();
+			var ImageSource = new UriImageSource
+            {
+                Uri = new System.Uri("http://lorempixel.com/1920/1080/city/1/"),
+                CachingEnabled = false,
+                CacheValidity = TimeSpan.FromHours(1)
+            };
+
+			i1.Source = ImageSource;
+
+        }
+    }
+}
