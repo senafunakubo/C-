09e4246b130d9470fd7e69720bb693229c15f251
Sena <inabacker24@gmail.com>
2017-10-05 00:11:52 -0700

    Add a test file

---
+++ Testtt/Testtt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Testtt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    Console.Write("matrix[{0},{1}] = ", row, column);
+                    matrix[row, column] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
